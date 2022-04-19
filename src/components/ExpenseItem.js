7ed1100eb4390fae413e4f823e95b6df18f9b642
Madison Shimizu <mhshimizu@outlook.com>
2022-04-19 11:05:09 -0500

    edits on ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,13 @@
-function ExpenseItem () {
-    
-    return (
-        <h2>Expense Item!</h2>
-    )
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
