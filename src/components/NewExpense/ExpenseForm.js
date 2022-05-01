a54e46c0f1768cb2f0fa7e6cb297da78199f3844
Madison Shimizu <mhshimizu@outlook.com>
2022-05-01 14:40:53 -0500

    added toggle for add expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = ({onSaveExpenseData}) => {
+const ExpenseForm = props => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -46,7 +46,7 @@ const ExpenseForm = ({onSaveExpenseData}) => {
       date: new Date(enteredDate),
     };
 
-    onSaveExpenseData(expenseData);
+    props.onSaveExpenseData(expenseData);
 
     setEnteredTitle("");
     setEnteredAmount("");
@@ -86,6 +86,7 @@ const ExpenseForm = ({onSaveExpenseData}) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,28 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       id: Math.random().toString(),
       ...enteredExpenseData,
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false)
   };
+
+  const editingHandler = () => {
+    isEditing ? setIsEditing(false) : setIsEditing(true)
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      
+      {!isEditing && <button onClick={editingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={editingHandler}/>}
     </div>
   );
 };
